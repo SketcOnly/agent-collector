f372ac1c606beef1af96cc07d095f5b94e12f973
sush-sketc <sshrise206516@163.com>
2025-11-18 21:40:30 +0800

    修改banner调用

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -3,10 +3,10 @@ package agent
 import (
 	"fmt"
 	"github.com/agent-collector/config"
-	"github.com/agent-collector/internal/registers"
-	"github.com/agent-collector/internal/server"
-	"github.com/agent-collector/pkg"
-	"github.com/agent-collector/pkg/logger"
+	"github.com/agent-collector/logger"
+	"github.com/agent-collector/registers"
+	"github.com/agent-collector/server"
+	"github.com/agent-collector/util"
 	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 	"os"
@@ -18,43 +18,44 @@ func Run(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-
+	
 	// 2，初始化banner
 	projectName := "agent-collector"
 	bannerColor := "ColorBlue" // 整体统一颜色
-	pkg.PrintBanner(projectName, bannerColor)
-
+	// PrintBanner(projectName, bannerColor)
+	util.PrintBanner(projectName, bannerColor)
+	
 	// 3. 设置全局默认 collector 为 complet（主程序相关日志自动使用）
 	logger.SetDefaultCollector("complete")
 	collector := logger.GetDefaultCollector()
-
+	
 	// 4, 设置当前goid
 	//logger.SetDefaultGid(strconv.FormatUint(goid.GetGID(), 10))
 	//goroutineid := logger.GetDefaultGid()
-
+	
 	logger.Info("log initialization successful ", collector, zap.String("path", cfg.Log.Path), zap.String("level", cfg.Log.Level),
 		zap.String("format", cfg.Log.Format))
 	logger.Debug("configuration initialization successful", collector, zap.String("path", c.String("config")))
-
+	
 	// 初始化 Prometheus 注册器
 	registry := registers.InitPromeReaistry(false, cfg)
-
+	
 	// 7. 启动采集器
 	//server.Start(context.Background())
-
+	
 	// 6. 初始化 HTTP 服务（注入配置、collector、注册器）
 	httpServer := server.NewHTTPServer(cfg.HTTP.Addr, registry)
 	if err := httpServer.Start(); err != nil {
 		return fmt.Errorf("start HTTP server failed: %w", err)
 	}
-
+	
 	server.WaitForShutdown(collector, func() error {
 		logger.Info("starting graceful shutdown...", "")
 		// 调用HTTP服务的Shutdown方法优雅关闭
 		if err := httpServer.Shutdown(); err != nil {
 			return fmt.Errorf("shutdown HTTP server failed: %w", err)
 		}
-
+		
 		// 步骤2：关闭采集器（若启动了，补充关闭逻辑）
 		// if err := collectorServer.Shutdown(context.Background()); err != nil {
 		//     logger.Error(collector, goroutineID, "collector server shutdown failed", zap.Error(err))
@@ -66,7 +67,7 @@ func Run(c *cli.Context) error {
 		}
 		logger.Info("all services shutdown successfully", "")
 		return nil
-
+		
 	})
 	return nil
 }
@@ -83,7 +84,7 @@ func InitDefaultLogger() error {
 
 func LoadConfig(c *cli.Context) (*config.Config, error) {
 	s := c.String("config")
-
+	
 	// 验证配置文件是否存在
 	if _, err := os.Stat(s); os.IsNotExist(err) {
 		return nil, fmt.Errorf("config file %s does not exist", s)
